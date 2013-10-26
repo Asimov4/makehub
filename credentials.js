96847847a94c883fbd061b8aafe7a1f75ead817a
stefania11 <druga.stefania@gmail.com>
2013-10-26 11:15:03 +0200

    Update credentials.js
    testing update of credentials

--- credentials.js
+++ credentials.js
@@ -1,4 +1,6 @@
 module.exports = {
-    GITHUB_CLIENT_ID: "", // Insert github client id here.
-    GITHUB_CLIENT_SECRET: ""  // Insert github client secret here.
+    GITHUB_CLIENT_ID: "da15939376d643d7adb8
+", // Insert github client id here.
+    GITHUB_CLIENT_SECRET: "ee2e35e26729226d1b6549ce354cbefd0ff97098
+"  // Insert github client secret here.
 };
